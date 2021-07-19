84ddc79a75ae97afa0a2c30781494856f65afbbf
Alexey <groznovaa@gmail.comls>
2021-07-19 18:11:12 +0500

    Add value account to run

--- src/main/java/com/javastart/hibernatetest/Application.java
+++ src/main/java/com/javastart/hibernatetest/Application.java
@@ -1,5 +1,9 @@
 package com.javastart.hibernatetest;
 
+import com.javastart.hibernatetest.dao.AccountDAO;
+import com.javastart.hibernatetest.entity.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
@@ -7,8 +11,17 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
         JpaRepositoriesAutoConfiguration.class})
-public class Application {
+public class Application implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    private AccountDAO accountDAO;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Account account = new Account("Lori", 15);
+
     }
 }
