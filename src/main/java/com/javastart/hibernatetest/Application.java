1caaa4029be211a1c1132c6d9346aab144d93ab0
Alexey <groznovaa@gmail.comls>
2021-07-19 18:05:52 +0500

    Add exlude to @SpringBootApplication

--- src/main/java/com/javastart/hibernatetest/Application.java
+++ src/main/java/com/javastart/hibernatetest/Application.java
@@ -2,8 +2,11 @@ package com.javastart.hibernatetest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
+        JpaRepositoriesAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
